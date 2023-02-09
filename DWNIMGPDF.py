5095993299960b36f746003600747fd04316d5af
Killlur <kavyamranjan17@gmail.com>
2023-02-09 21:02:41 +0530

    fixed typos , small bugs

--- DWNIMGPDF.py
+++ DWNIMGPDF.py
@@ -13,18 +13,23 @@ chapter=input('Write Chapter name and number in format[Number-Name] - ')
 print('Checking Chapter Name...')
 
 f = ""
-for url in search((chapter+" NCERT Solution Sanskrit Class 10 Knowledge Gallery") , stop=1):
-    f = url
+for url in search((f" NCERT Solution for Class 10 Sanskrit chapter {chapter} Knowledge Gallery") , stop=3):
+    f = url if (url[:72] == "https://knowledgegallery.in/ncert-solution-for-class-10-sanskrit-chapter") else f
 wwn = f[73:len(f)-1]
 suggestion = wwn.split('-')[0] +"-"+ ('-').join(i.capitalize() for i in wwn.split('-')[1:])
 
 if suggestion == chapter:
     pass
+elif suggestion == '7-Sauhardam-Prakrite-Shobha': #Exception
+    y = input(f"We Suggest changing chapter name to 7-Souhadra-Prakrite-Shobha ? (y/n) - ")
+    if y in ("y yes yup yeah sure").split():
+        chapter = '7-Souhadra-Prakrite-Shobha'
+        print(f"Chapter name changed to {chapter}...")
 else:
     y = input(f"We Suggest changing chapter name to {suggestion} ? (y/n) - ")
     if y in ("y yes yup yeah sure").split():
         chapter = suggestion
-        print(f"Chapter name changes to {chapter}")
+        print(f"Chapter name changed to {chapter}...")
 
 m=0
 newdir= dir+'/'+chapter+('_').join((('_').join(x for x in (i.split(':'))) for i in str(datetime.datetime.now())[0:-7].split(' ')))
@@ -47,7 +52,7 @@ for i in range(1,200):
     except Exception:
         os.remove(img)
         if i ==1:
-            print('Either >=10 or Incorrect Chapter Name')
+            print('Either >=10 or Incorrect Chapter Name ...')
             for x in range(1, 200):
                 try:
                     img = newdir + f'/{chapter}-Pg-{x}.png'
@@ -70,7 +75,7 @@ for i in range(1,200):
             break
         else:
             break
-print('Number Of Pages -%s'%m)
+print('Number Of Pages - %s'%m)
 print(f'Converting into pdf - {newdir}/{chapter}.pdf...')
 
 finallist=[]
